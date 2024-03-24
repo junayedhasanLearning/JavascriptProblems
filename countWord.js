4d740650bed8cdd224c5d55c9a6e7f6746f4e0c8
junayedhasanLearning <junayedhasan0271@gmail.com>
2024-03-24 20:15:07 +0600

    Count the number of words in a string

---
+++ countWord.js
@@ -0,0 +1,16 @@
+var count = 0;
+
+function wordCount(speech){
+    for(let i = 0; i < speech.length; i++) {
+        var element = speech[i];
+        if (element == " " && speech[i-1] != " "){
+            count++
+        }
+    }
+    return count;    
+}
+
+var speech = "I   am Junayed Hasan Sakib. I am here to learn programming."
+var counting = wordCount(speech);
+
+console.log(`The total word is : ${counting}`);
